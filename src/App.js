b7c89f31886885c1bbd67a69db0f9b377346d21b
luk3my <luk3myoung@gmail.com>
2021-09-24 21:55:18 +1000

    add tasks to state as props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from 'react'
 
 function App() {
+   const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'task1',
+            day: 'today',
+            reminder: true 
+        },{
+            id: 2,
+            text: 'task2',
+            day: 'tomorrow',
+            reminder: true 
+        },{
+            id: 3,
+            text: 'task3',
+            day: 'next week',
+            reminder: false
+        }
+    ])
   return (
     <div className="container">
       <Header title='Task Tracker'/>
+      <Tasks tasks={tasks} />
     </div>
   );
 }
