e3c4305394a4ad6fdb2d512b7aa5cc56e8329f45
luk3my <luk3myoung@gmail.com>
2021-09-26 20:23:20 +1000

    toggle btn add/close

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 import Button from './Button.js'
 
-const Header = ({ title, onAdd }) => {
+const Header = ({ title, onAdd, showAdd }) => {
     return (
         <header className="header">
             <h1>{title}</h1>
-            <Button color='Green' text='Add' onClick={onAdd}/>
+            <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
         </header>
     )
 }
